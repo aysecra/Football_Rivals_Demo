5c8deefe1e588fefbacbadcc52811ece80e4a1eb
Ayşe Karataş <72119012+aysecra@users.noreply.github.com>
2023-08-01 19:20:12 +0300

    Subbuttons are added

--- Assets/AyseKaratas_KumkuatDemo/Code/Scripts/Managers/GUIManager.cs
+++ Assets/AyseKaratas_KumkuatDemo/Code/Scripts/Managers/GUIManager.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GUIManager : MonoBehaviour
+namespace KumkuatDemo
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GUIManager : Singleton<GUIManager>
     {
-        
-    }
+        [SerializeField] private GameObject _teamArea;
+        [SerializeField] private GameObject _teamMemberArea;
+        [SerializeField] private GameObject _teamMatchArea;
+        [SerializeField] private GameObject _teamAttackArea;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        private void Start()
+        {
+            OpenTeamArea();
+        }
+
+        private void OpenTeamArea()
+        {
+            _teamArea.SetActive(true);
+            _teamMemberArea.SetActive(false);
+            _teamMatchArea.SetActive(false);
+            _teamAttackArea.SetActive(false);
+        }
+
+        private void OpenTeamMemberArea()
+        {
+            _teamArea.SetActive(false);
+            _teamMemberArea.SetActive(true);
+            _teamMatchArea.SetActive(false);
+            _teamAttackArea.SetActive(false);
+        }
+
+        private void OpenMatchArea()
+        {
+            _teamArea.SetActive(false);
+            _teamMemberArea.SetActive(true);
+            _teamMatchArea.SetActive(false);
+            _teamAttackArea.SetActive(false);
+        }
+
+        private void OpenAttackArea()
+        {
+            _teamArea.SetActive(false);
+            _teamMemberArea.SetActive(false);
+            _teamMatchArea.SetActive(false);
+            _teamAttackArea.SetActive(true);
+        }
     }
 }
